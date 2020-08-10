6000026c9a7a2864684a53675cef8ac0d55c13f9
maelie poussier <maelie.poussier@icloud.com>
2020-08-10 15:54:43 +0200

    longest word game

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,6 @@
+require 'open-uri'
+require 'json'
+
 class PagesController < ApplicationController
   def new
     @letters = []
@@ -6,5 +9,26 @@ class PagesController < ApplicationController
   end
 
   def score
+    @word = params[:word].upcase
+    @grid = params[:grid]
+    grid_letters = @grid.each_char { |letter| print letter, '' }
+    if letter_in_grid == false
+      @result = "Sorry, but #{@word.upcase} can’t be built out of #{grid_letters}."
+    elsif english_word == false
+      @result = "Sorry but #{@word.upcase} does not seem to be an English word."
+    elsif letter_in_grid && english_word
+      @result = "Congratulation! #{@word.upcase} is a valid English word."
+    end
+  end
+
+  def letter_in_grid
+    @word.chars.all? { |letter| @grid.include?(letter) }
+  end
+
+  def english_word
+    url = "https://wagon-dictionary.herokuapp.com/#{@word}"
+    word_dictionary = open(url).read
+    word = JSON.parse(word_dictionary)
+    return word['found']
   end
 end
